a0bedbe3b319dd0c81c93ba25d76ebf845f41db4
j.l <jaani.leskinen@gmail.com>
2014-09-25 17:35:40 +0300

    database.sql loitsut

--- database.sql
+++ database.sql
@@ -1,7 +1,23 @@
-CREATE DATABASE mediaplayer; -- luo tietokanta
+CREATE DATABASE mediaplayer;
 
-USE mediaplayer; -- käytä tietokantaa
+USE mediaplayer;
 
-CREATE TABLE kappale (id INT PRIMARY KEY, pituus VARCHAR(20),
- artisti VARCHAR(50), genre VARCHAR(20), nimi VARCHAR(50),
- linkki VARCHAR(100)); -- luo taulu
+CREATE TABLE biisit(
+    ID INT PRIMARY KEY,
+    nimi VARCHAR(50),
+    pituus VARCHAR(20));
+
+CREATE TABLE kayttajat(
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    tunnus VARCHAR(20),
+    salasana VARCHAR(30));
+
+CREATE TABLE soittolistat(
+    biisiID INT,
+    tunnusID INT,
+    FOREIGN KEY (biisiID) REFERENCES biisit(ID),
+    FOREIGN KEY (tunnusID) REFERENCES kayttajat(ID),
+    PRIMARY KEY (biisiID, tunnusID));
+
+INSERT INTO kayttajat (tunnus, salasana) VALUES("mediaplayer","1234");
+
